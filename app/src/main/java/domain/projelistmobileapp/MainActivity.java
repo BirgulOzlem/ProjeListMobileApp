24ac68a0e4044a5c7794c870ddebb1c1ee5c343b
Birgül Özlem <birgulozlemm@gmail.com>
2015-04-09 22:58:42 +0300

    Share Button is working
    ** action_provider_class.xml file deleted
    
    ** share button had added to action bar with new item in
    main_activity_actions.xml

--- app/src/main/java/domain/projelistmobileapp/MainActivity.java
+++ app/src/main/java/domain/projelistmobileapp/MainActivity.java
@@ -6,21 +6,20 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
     private Context activity;
-
-    //private ShareActionProvider mShareActionProvider;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,69 +29,27 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
-
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         MenuItem shareItem = menu.findItem(R.id.action_share);
-
-        ShareActionProvider mShareActionProvider = new ShareActionProvider(getActivity());
-        MenuItemCompat.setActionProvider(shareItem, mShareActionProvider);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        Log.w("NullPointerException", "Is mShareActionProvider null: " + (mShareActionProvider == null));
+        mShareActionProvider.setShareIntent(getDefaultIntent());
 
         return super.onCreateOptionsMenu(menu);
     }
 
-    private Intent getDefaultIntent() {
+    private Intent getDefaultIntent()
+    {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("image/*");
         return intent;
     }
 
-    public View onCreateActionView(MenuItem forItem) {
-        // Inflate the action view to be shown on the action bar.
-        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-        View view = layoutInflater.inflate(R.layout.action_provider, null);
-        ImageButton button = (ImageButton) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Do something...
-            }
-        });
-        return view;
-    }
-
-
     @Override
-    public View onCreateActionView() {
-        // Inflate the action view to be shown on the action bar.
-        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-        View view = layoutInflater.inflate(R.layout.action_provider, null);
-        ImageButton button = (ImageButton) view.findViewById(R.id.menuButton);
-        // Attach a click listener for launching the system settings.
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(mContext, v);
-                popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(mContext, "Clicked popup menu item " + item.getTitle(),
-                                Toast.LENGTH_SHORT).show();
-                        return true;
-                    }
-                });
-                popup.show();
-                Toast.makeText(mContext, "works", Toast.LENGTH_SHORT).show();
-                //mContext.startActivity(sSettingsIntent);
-            }
-        });
-        return view;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
             case R.id.action_search:
@@ -118,7 +75,8 @@ public class MainActivity extends ActionBarActivity {
         // TO DO
     }
 
-    public Context getActivity() {
+    public Context getActivity()
+    {
         return activity;
     }
 }
