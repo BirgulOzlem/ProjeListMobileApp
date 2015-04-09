8389df900d471aea64b6e38a5d4d8fbe03432030
Birgül Özlem <birgulozlemm@gmail.com>
2015-04-09 17:31:43 +0300

    ShareActionProvider is ok
    **onCreateOptonMenu is updated
    
    **null pointer exception fatal error is debuged
    
    ** still there is no icon tor share,

--- app/src/main/java/domain/projelistmobileapp/MainActivity.java
+++ app/src/main/java/domain/projelistmobileapp/MainActivity.java
@@ -1,19 +1,23 @@
 package domain.projelistmobileapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-//import com.actionbarsherlock.widget.ShareActionProvider;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Context activity;
 
-    private ShareActionProvider mShareActionProvider;
+    //private ShareActionProvider mShareActionProvider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,10 +34,10 @@ public class MainActivity extends ActionBarActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
 
-        // Set up ShareActionProvider's default share intent
         MenuItem shareItem = menu.findItem(R.id.action_share);
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
-        mShareActionProvider.setShareIntent(getDefaultIntent());
+
+        ShareActionProvider mShareActionProvider = new ShareActionProvider(getActivity());
+        MenuItemCompat.setActionProvider(shareItem, mShareActionProvider);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -42,6 +46,49 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("image/*");
         return intent;
+    }
+
+    public View onCreateActionView(MenuItem forItem) {
+        // Inflate the action view to be shown on the action bar.
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View view = layoutInflater.inflate(R.layout.action_provider, null);
+        ImageButton button = (ImageButton) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something...
+            }
+        });
+        return view;
+    }
+
+
+    @Override
+    public View onCreateActionView() {
+        // Inflate the action view to be shown on the action bar.
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View view = layoutInflater.inflate(R.layout.action_provider, null);
+        ImageButton button = (ImageButton) view.findViewById(R.id.menuButton);
+        // Attach a click listener for launching the system settings.
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(mContext, v);
+                popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(mContext, "Clicked popup menu item " + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popup.show();
+                Toast.makeText(mContext, "works", Toast.LENGTH_SHORT).show();
+                //mContext.startActivity(sSettingsIntent);
+            }
+        });
+        return view;
     }
 
     @Override
@@ -71,4 +118,7 @@ public class MainActivity extends ActionBarActivity {
         // TO DO
     }
 
+    public Context getActivity() {
+        return activity;
+    }
 }
